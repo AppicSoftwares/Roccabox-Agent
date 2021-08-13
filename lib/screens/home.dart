f8450352b54955b07d2d8b120b2f5bba27ea947a
Sagar Shrivastava <er.sagarshrivastava@hotmail.com>
2021-08-13 12:04:49 +0530

    Logout

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'login.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,13 +11,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
+        actions: [
+          IconButton(
+           icon: Icon(Icons.exit_to_app), onPressed: () {
+              Logout();
+          }, )
+        ],
       ),
 
     );
+  }
+
+  Future<void> Logout() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.clear();
+    preferences.commit();
+    Navigator.pushAndRemoveUntil(context,
+        MaterialPageRoute(builder: (context) => Login()), (r)=>false);
   }
 }
