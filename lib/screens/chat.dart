af5e4e4e65f53062bcb7eaa3394a3e87ce0f854c
Sagar Shrivastava <er.sagarshrivastava@hotmail.com>
2021-09-20 16:18:34 +0530

    chat complete

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,7 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:roccabox_agent/screens/chatscreen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class Chat extends StatelessWidget {
+class Chat extends StatefulWidget {
+  @override
+  State<Chat> createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> {
+  AsyncSnapshot<QuerySnapshot>? snapshot;
+  List<QueryDocumentSnapshot> listUsers = new List.from([]);
+  final firestoreInstance = FirebaseFirestore.instance;
+  List<UserList> listUser = [];
+  var id;
+  @override
+  void initState() {
+    getData();
+    WidgetsBinding.instance!.addObserver(
+        LifecycleEventHandler(resumeCallBack: () async {
+          print("Invoke");
+        }
+    ));
+
+    super.initState();
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,34 +42,147 @@ class Chat extends StatelessWidget {
               fontWeight: FontWeight.w600),
         ),
       ),
-      body: ListView.separated(
-        itemCount: 15,
-        separatorBuilder: (BuildContext context, int index) {
-          return Divider(
-            color: Color(0xff707070),
+      body: id!=null?StreamBuilder<QuerySnapshot>(
+        stream: firestoreInstance
+            .collection("chat_master")
+            .doc("chat_head")
+            .collection(id)
+            .snapshots(),
+        builder:(BuildContext context,
+    AsyncSnapshot<QuerySnapshot> snapshot) {
+          if(snapshot.hasData){
+            listUser.clear();
+            snapshot.data?.docs.forEach((element) {
+              var json;
+              UserList model = new UserList();
+              model.id = element.id.toString();
+
+              print("StreamId "+element.id);
+              json = element.data();
+                print("StreamName " + json["user"].toString());
+                print("StreamImage " + element.get("image").toString());
+                model.name = json["user"].toString();
+                model.image = json["image"].toString();
+                model.message = json["msg"].toString();
+
+              listUser.add(model);
+            });
+          }
+          return ListView.separated(
+            itemCount: listUser.length,
+            separatorBuilder: (BuildContext context, int index) {
+              return Divider(
+                color: Color(0xff707070),
+              );
+            },
+
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                onTap: () =>
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (context) =>
+                        ChatScreen(name: listUser[index].name,
+                          image: listUser[index].image,
+                          receiverId: listUser[index].id,
+                          senderId: id,))),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/img1.png'),
+                ),
+                title: Text(
+                  listUser == null ? "" : listUser[index].name.toString(),
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xff000000)),
+                ),
+                subtitle: Text(
+                  listUser == null ? "" : listUser[index].message == null
+                      ? ""
+                      : listUser[index].message.toString(),
+                  style: TextStyle(fontSize: 12, color: Color(0xff818181)),
+                ),
+              );
+            },
           );
-        },
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            onTap: () => Navigator.push(
-                context, MaterialPageRoute(builder: (context) => ChatScreen())),
-            leading: CircleAvatar(
-              backgroundImage: AssetImage('assets/img1.png'),
-            ),
-            title: Text(
-              'Assigned Agent',
-              style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                  color: Color(0xff000000)),
-            ),
-            subtitle: Text(
-              'Sukhdev Place',
-              style: TextStyle(fontSize: 12, color: Color(0xff818181)),
-            ),
-          );
-        },
-      ),
+
+        }
+      ):Text("No Data Found"),
     );
+  }
+
+  void getData() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    id = pref.getString("id");
+    setState(() {
+
+    });
+   /* firestoreInstance.collection("chat_master").get().then((querySnapshot) {
+      querySnapshot.docs.forEach((result) {
+        firestoreInstance
+            .collection("chat_master")
+            .doc(result.id)
+            .collection(id.toString())
+            .get()
+            .then((querySnapshot) {
+              querySnapshot.docs.forEach((element) {
+                UserList model = new UserList();
+                model.id = element.id.toString();
+                model.name = element.data()["user"].toString();
+                model.image = element.data()["image"].toString();
+                model.message = element.data()["msg"].toString();
+                print("Id "+element.id);
+                print("DataName "+element.data()["user"].toString());
+                print("DataImage "+element.data()["image"].toString());
+
+                //print("Name "+element.id);
+                listUser.add(model);
+              });
+              setState(() {
+
+              });
+          *//*  listUsers.clear();
+            listUsers.addAll(snapshot!.data!.docs);
+            print("LengthofList "+listUsers.length.toString()+"");
+           *//*   *//*querySnapshot.docs.forEach((result) {
+            print(result.data());
+          });*//*
+        });
+      });
+    });*/
+    
+  }
+}
+
+
+class UserList{
+  String? name;
+  String? id;
+  String? image;
+  String? message;
+}
+
+
+class LifecycleEventHandler extends WidgetsBindingObserver {
+  final AsyncCallback resumeCallBack;
+
+
+  LifecycleEventHandler({
+    required this.resumeCallBack,
+
+  });
+
+  @override
+  Future<void> didChangeAppLifecycleState(AppLifecycleState state) async {
+    switch (state) {
+      case AppLifecycleState.resumed:
+        if (resumeCallBack != null) {
+          await resumeCallBack();
+        }
+        break;
+      case AppLifecycleState.inactive:
+      case AppLifecycleState.paused:
+      case AppLifecycleState.detached:
+        break;
+    }
   }
 }
